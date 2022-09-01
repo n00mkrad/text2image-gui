c1c67e1bb2af1f6577bc10a8bfba8143f36d8581
n00mkrad <login@nmkd.de>
2022-09-01 09:36:29 +0200

    Disable weird DPI scaling

--- StableDiffusionGui/Properties/AssemblyInfo.cs
+++ StableDiffusionGui/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Media;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -13,6 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: DisableDpiAwareness]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
