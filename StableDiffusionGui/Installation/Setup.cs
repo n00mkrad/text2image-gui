8ae0e219d271fdce44d3b03b502f7fb3b17e62f2
n00mkrad <login@nmkd.de>
2022-09-08 21:49:08 +0200

    Fixed GFPGAN install path

--- StableDiffusionGui/Installation/Setup.cs
+++ StableDiffusionGui/Installation/Setup.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Management.Automation;
 using System.Threading.Tasks;
 
 namespace StableDiffusionGui.Installation
@@ -265,7 +266,7 @@ namespace StableDiffusionGui.Installation
                 if (print)
                     Logger.Log("Installing GFPGAN...");
 
-                string gfpganPath = Path.Combine("GFPGAN");
+                string gfpganPath = GetDataSubPath("GFPGAN");
                 IoUtils.SetAttributes(gfpganPath, FileAttributes.Normal);
 
                 if (Directory.Exists(gfpganPath))
