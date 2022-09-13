d92e19dbcb8ca13f0a76319d40bb6765d6d76852
n00mkrad <login@nmkd.de>
2022-09-13 12:08:37 +0200

    Split TtiProcess OutputHandler into own class, restart dream.py only when needed

--- StableDiffusionGui/Main/TtiProcess.cs
+++ StableDiffusionGui/Main/TtiProcess.cs
@@ -1,7 +1,5 @@
 ﻿using StableDiffusionGui.Io;
-using StableDiffusionGui.MiscUtils;
 using StableDiffusionGui.Os;
-using StableDiffusionGui.Ui;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,17 +14,13 @@ namespace StableDiffusionGui.Main
     {
         private static Process _dreamPy;
         public static bool IsDreamPyRunning { get { return _dreamPy != null && !_dreamPy.HasExited; } }
-        private static bool _hasErrored = false;
-
-        public static void Start()
-        {
-            _hasErrored = false;
-        }
 
         public static void Finish()
         {
             return;
         }
+
+        private static string _lastDreamPyStartupSettings;
 
         public static async Task RunStableDiffusion(string[] prompts, string initImg, string embedding, float[] initStrengths, int iterations, int steps, float[] scales, long seed, string sampler, Size res, bool seamless, string outPath)
         {
@@ -83,8 +77,20 @@ namespace StableDiffusionGui.Main
 
             Logger.Log($"Running Stable Diffusion - {iterations} Iterations, {steps} Steps, Scales {(scales.Length < 4 ? string.Join(", ", scales.Select(x => x.ToStringDot())) : $"{scales.First()}->{scales.Last()}")}, {res.Width}x{res.Height}, Starting Seed: {startSeed}");
 
-            if (!IsDreamPyRunning)
+            string mdlArg = GetSdModel();
+            string precArg = $"{(Config.GetBool("checkboxFullPrecision") ? "-F" : "")}";
+            string embArg = !string.IsNullOrWhiteSpace(embedding) ? $"--embedding_path {embedding.Wrap()}" : "";
+            string deviceArg = TtiUtils.GetCudaDevice("--device");
+
+            string newStartupSettings = $"{mdlArg}{precArg}{embArg}{deviceArg}"; // Check if startup settings match - If not, we need to reload the model
+
+            string strengths = File.Exists(initImg) ? $" and {initStrengths.Length} strength{(initStrengths.Length != 1 ? "s" : "")}" : "";
+            Logger.Log($"{prompts.Length} prompt{(prompts.Length != 1 ? "s" : "")} with {iterations} iteration{(iterations != 1 ? "s" : "")} each and {scales.Length} scale{(scales.Length != 1 ? "s" : "")}{strengths} each = {TextToImage.CurrentTask.TargetImgCount} images total.");
+
+            if (!IsDreamPyRunning || (IsDreamPyRunning && _lastDreamPyStartupSettings != newStartupSettings))
             {
+                _lastDreamPyStartupSettings = newStartupSettings;
+
                 if (!string.IsNullOrWhiteSpace(embedding))
                 {
                     if (!File.Exists(embedding))
@@ -93,28 +99,23 @@ namespace StableDiffusionGui.Main
                         Logger.Log($"Using learned concept: {Path.GetFileName(embedding)}");
                 }
 
-                string strengths = File.Exists(initImg) ? $" and {initStrengths.Length} strength{(initStrengths.Length != 1 ? "s" : "")}" : "";
-                Logger.Log($"{prompts.Length} prompt{(prompts.Length != 1 ? "s" : "")} with {iterations} iteration{(iterations != 1 ? "s" : "")} each and {scales.Length} scale{(scales.Length != 1 ? "s" : "")}{strengths} each = {TextToImage.CurrentTask.TargetImgCount} images total.");
-
                 Process dream = OsUtils.NewProcess(!OsUtils.ShowHiddenCmd());
                 TextToImage.CurrentTask.Processes.Add(dream);
 
-                string prec = $"{(Config.GetBool("checkboxFullPrecision") ? "-F" : "")}";
-
                 dream.StartInfo.Arguments = $"{OsUtils.GetCmdArg()} cd /D {Paths.GetDataPath().Wrap()} && call \"{Paths.GetDataPath()}\\mb\\Scripts\\activate.bat\" ldo && " +
-                    $"python \"{Paths.GetDataPath()}/repo/scripts/dream.py\" --model {GetSdModel()} -o {outPath.Wrap()} --from_file_loop={promptFilePath.Wrap()} {prec} " +
-                    $"{(!string.IsNullOrWhiteSpace(embedding) ? $"--embedding_path {embedding.Wrap()}" : "")} {TtiUtils.GetCudaDevice("--device")} --print_steps ";
+                    $"python \"{Paths.GetDataPath()}/repo/scripts/dream.py\" --model {GetSdModel()} -o {outPath.Wrap()} --from_file_loop={promptFilePath.Wrap()} {precArg} " +
+                    $"{embArg} {deviceArg} --print_steps ";
 
                 Logger.Log("cmd.exe " + dream.StartInfo.Arguments, true);
 
                 if (!OsUtils.ShowHiddenCmd())
                 {
-                    dream.OutputDataReceived += (sender, line) => { LogOutput(line.Data); };
-                    dream.ErrorDataReceived += (sender, line) => { LogOutput(line.Data, true); };
+                    dream.OutputDataReceived += (sender, line) => { TtiProcessOutputHandler.LogOutput(line.Data); };
+                    dream.ErrorDataReceived += (sender, line) => { TtiProcessOutputHandler.LogOutput(line.Data, true); };
                 }
 
                 ProcessManager.FindAndKillOrphans("dream.py");
-                Start();
+                TtiProcessOutputHandler.Start();
                 Logger.Log("Loading Stable Diffusion...");
                 _dreamPy = dream;
                 dream.Start();
@@ -177,12 +178,12 @@ namespace StableDiffusionGui.Main
 
             if (!OsUtils.ShowHiddenCmd())
             {
-                dream.OutputDataReceived += (sender, line) => { LogOutput(line.Data); };
-                dream.ErrorDataReceived += (sender, line) => { LogOutput(line.Data, true); };
+                dream.OutputDataReceived += (sender, line) => { TtiProcessOutputHandler.LogOutput(line.Data); };
+                dream.ErrorDataReceived += (sender, line) => { TtiProcessOutputHandler.LogOutput(line.Data, true); };
             }
 
             ProcessManager.FindAndKillOrphans("dream.py");
-            Start();
+            TtiProcessOutputHandler.Start();
             Logger.Log("Loading Stable Diffusion...");
             dream.Start();
 
@@ -233,144 +234,6 @@ namespace StableDiffusionGui.Main
         {
             string filename = Config.Get(Config.Key.comboxSdModel);
             return withExtension ? filename : Path.GetFileNameWithoutExtension(filename);
-        }
-
-        static void LogOutput(string line, bool stdErr = false)
-        {
-            if (string.IsNullOrWhiteSpace(line))
-                return;
-
-            //Stopwatch sw = new Stopwatch();
-            //sw.Restart();
-
-            //lastLogName = ai.LogFilename;
-            Logger.Log(line, true, false, "sd");
-
-            bool ellipsis = Logger.LastUiLine.Contains("...");
-
-            if (TextToImage.LastTaskSettings != null && TextToImage.LastTaskSettings.Implementation == Data.Implementation.StableDiffusion)
-            {
-                bool replace = ellipsis || Logger.LastUiLine.MatchesWildcard("*Generated*image*in*");
-
-                if (line.Contains("Setting Sampler"))
-                    Logger.Log("Generating...");
-
-                if (line.MatchesWildcard("step */*"))
-                {
-                    int[] stepsCurrentTarget = line.Split("step ")[1].Split('/').Select(x => x.GetInt()).ToArray();
-
-                    int percent = (((float)stepsCurrentTarget[0] / stepsCurrentTarget[1]) * 100f).RoundToInt();
-
-                    if (percent > 0 && percent <= 100)
-                    {
-                        //Logger.Log($"Generating... {percent}%", false, replace);
-                        Program.MainForm.SetProgressImg(percent);
-                    }
-                }
-
-                if (line.Contains("image(s) generated in "))
-                {
-                    var split = line.Split("image(s) generated in ");
-                    TextToImage.CurrentTask.ImgCount += split[0].GetInt();
-                    Program.MainForm.SetProgress((int)Math.Round(((float)TextToImage.CurrentTask.ImgCount / TextToImage.CurrentTask.TargetImgCount) * 100f));
-
-                    int lastMsPerImg = $"{split[1].Remove(".").Remove("s")}0".GetInt();
-                    int remainingMs = (TextToImage.CurrentTask.TargetImgCount - TextToImage.CurrentTask.ImgCount) * lastMsPerImg;
-
-                    string lastLine = Logger.LastUiLine;
-
-                    Logger.Log($"Generated {split[0].GetInt()} image in {split[1]} ({TextToImage.CurrentTask.ImgCount}/{TextToImage.CurrentTask.TargetImgCount})" +
-                        $"{(TextToImage.CurrentTask.ImgCount > 1 && remainingMs > 1000 ? $" - ETA: {FormatUtils.Time(remainingMs, false)}" : "")}", false, replace || Logger.LastUiLine.MatchesWildcard("*Generated*image*in*"));
-                }
-            }
-
-            if (TextToImage.LastTaskSettings != null && TextToImage.LastTaskSettings.Implementation == Data.Implementation.StableDiffusionOptimized)
-            {
-                bool replace = ellipsis || Logger.LastUiLine.MatchesWildcard("*Generated*image*in*");
-
-                if (line.Contains("reading prompts from"))
-                    Logger.Log("Generating...");
-
-                if (line.Contains("Decoding image: "))
-                {
-                    int percent = line.Split("Decoding image: ")[1].Split('#')[0].GetInt();
-
-                    if(percent > 0 && percent <= 100)
-                    {
-                        //Logger.Log($"Generating... {percent}%", false, replace);
-                        Program.MainForm.SetProgressImg(percent);
-                    }
-                }
-
-                if (line.MatchesWildcard("*data: 100%*<00:00,*it*]"))
-                {
-                    TextToImage.CurrentTask.ImgCount += 1;
-                    Program.MainForm.SetProgress((int)Math.Round(((float)TextToImage.CurrentTask.ImgCount / TextToImage.CurrentTask.TargetImgCount) * 100f));
-
-                    int lastMsPerImg = line.EndsWith("it/s]") ? (1000000f / (line.Split("00:00, ").Last().Remove(".").Remove("s") + "0").GetInt()).RoundToInt() : (line.Split("00:00, ").Last().Remove(".").Remove("s") + "0").GetInt();
-                    int remainingMs = (TextToImage.CurrentTask.TargetImgCount - TextToImage.CurrentTask.ImgCount) * lastMsPerImg;
-
-                    string lastLine = Logger.LastUiLine;
-
-                    Logger.Log($"Generated 1 image in {FormatUtils.Time(lastMsPerImg, false)} ({TextToImage.CurrentTask.ImgCount}/{TextToImage.CurrentTask.TargetImgCount})" +
-                        $"{(TextToImage.CurrentTask.ImgCount > 1 && remainingMs > 1000 ? $" - ETA: {FormatUtils.Time(remainingMs, false)}" : "")}", false, replace || Logger.LastUiLine.MatchesWildcard("Generated*image*"));
-                }
-            }
-
-
-            if (line.MatchesWildcard("*%|*/*[*B/s]*") && !line.ToLower().Contains("it/s") && !line.ToLower().Contains("s/it"))
-            {
-                Logger.Log($"Downloading required files... {line.Trunc(80)}", false, ellipsis);
-            }
-
-            if (line.MatchesWildcard("Added terms: *, *"))
-            {
-                if(line.MatchesWildcard("*, <*>"))
-                    Logger.Log($"Concept keyword: {line.Split("Added terms: *, ").LastOrDefault()}", false, ellipsis);
-                else
-                    Logger.Log($"Concept keyword: <{line.Split("Added terms: *, ").LastOrDefault()}>", false, ellipsis);
-            }
-
-            string lastLogLines = string.Join("\n", Logger.GetSessionLogLastLines("sd", 6));
-
-            if (!_hasErrored && line.Contains("CUDA out of memory"))
-            {
-                _hasErrored = true;
-                UiUtils.ShowMessageBox($"Your GPU ran out of VRAM! Try a lower resolution.\n\n{line.Split("If reserved memory is").FirstOrDefault()}", UiUtils.MessageType.Error);
-            }
-
-            if (!_hasErrored && (line.Contains("RuntimeError") || line.Contains("ImportError") || line.Contains("OSError")))
-            {
-                _hasErrored = true;
-                UiUtils.ShowMessageBox($"Python Error:\n\n{lastLogLines}", UiUtils.MessageType.Error);
-            }
-
-            if (!_hasErrored && (line.Contains("PytorchStreamReader failed reading zip archive") || line.Contains("UnpicklingError")))
-            {
-                _hasErrored = true;
-                UiUtils.ShowMessageBox($"Your model file seems to be damaged or incomplete!\n\n{lastLogLines}", UiUtils.MessageType.Error);
-            }
-
-            if (!_hasErrored && line.Contains("usage: "))
-            {
-                _hasErrored = true;
-                UiUtils.ShowMessageBox($"Invalid CLI syntax.", UiUtils.MessageType.Error);
-            }
-
-            if (!_hasErrored && line.ToLower().Contains("illegal memory access"))
-            {
-                _hasErrored = true;
-                UiUtils.ShowMessageBox($"Your GPU appears to be unstable! If you have an overclock enabled, please disable it!\n\n{line}", UiUtils.MessageType.Error);
-            }
-
-            // if (!_hasErrored && line.ToLower().Contains("0 image(s) generated in"))
-            // {
-            //     _hasErrored = true;
-            //     UiUtils.ShowMessageBox($"An unknown error occured. Check the log for details:!\n\n{lastLogLines}", UiUtils.MessageType.Error);
-            // }
-
-            if (_hasErrored)
-                TextToImage.Cancel();
         }
 
         public static void Kill()
---
+++ StableDiffusionGui/Main/TtiProcessOutputHandler.cs
@@ -0,0 +1,158 @@
+﻿using StableDiffusionGui.MiscUtils;
+using StableDiffusionGui.Ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StableDiffusionGui.Main
+{
+    internal class TtiProcessOutputHandler
+    {
+        private static bool _hasErrored = false;
+
+        public static void Start()
+        {
+            _hasErrored = false;
+        }
+
+        public static void LogOutput(string line, bool stdErr = false)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+
+            //Stopwatch sw = new Stopwatch();
+            //sw.Restart();
+
+            //lastLogName = ai.LogFilename;
+            Logger.Log(line, true, false, "sd");
+
+            bool ellipsis = Logger.LastUiLine.Contains("...");
+
+            if (TextToImage.LastTaskSettings != null && TextToImage.LastTaskSettings.Implementation == Data.Implementation.StableDiffusion)
+            {
+                bool replace = ellipsis || Logger.LastUiLine.MatchesWildcard("*Generated*image*in*");
+
+                if (line.Contains("Setting Sampler"))
+                    Logger.Log("Generating...");
+
+                if (line.MatchesWildcard("step */*"))
+                {
+                    int[] stepsCurrentTarget = line.Split("step ")[1].Split('/').Select(x => x.GetInt()).ToArray();
+
+                    int percent = (((float)stepsCurrentTarget[0] / stepsCurrentTarget[1]) * 100f).RoundToInt();
+
+                    if (percent > 0 && percent <= 100)
+                    {
+                        //Logger.Log($"Generating... {percent}%", false, replace);
+                        Program.MainForm.SetProgressImg(percent);
+                    }
+                }
+
+                if (line.Contains("image(s) generated in "))
+                {
+                    var split = line.Split("image(s) generated in ");
+                    TextToImage.CurrentTask.ImgCount += split[0].GetInt();
+                    Program.MainForm.SetProgress((int)Math.Round(((float)TextToImage.CurrentTask.ImgCount / TextToImage.CurrentTask.TargetImgCount) * 100f));
+
+                    int lastMsPerImg = $"{split[1].Remove(".").Remove("s")}0".GetInt();
+                    int remainingMs = (TextToImage.CurrentTask.TargetImgCount - TextToImage.CurrentTask.ImgCount) * lastMsPerImg;
+
+                    string lastLine = Logger.LastUiLine;
+
+                    Logger.Log($"Generated {split[0].GetInt()} image in {split[1]} ({TextToImage.CurrentTask.ImgCount}/{TextToImage.CurrentTask.TargetImgCount})" +
+                        $"{(TextToImage.CurrentTask.ImgCount > 1 && remainingMs > 1000 ? $" - ETA: {FormatUtils.Time(remainingMs, false)}" : "")}", false, replace || Logger.LastUiLine.MatchesWildcard("*Generated*image*in*"));
+                }
+            }
+
+            if (TextToImage.LastTaskSettings != null && TextToImage.LastTaskSettings.Implementation == Data.Implementation.StableDiffusionOptimized)
+            {
+                bool replace = ellipsis || Logger.LastUiLine.MatchesWildcard("*Generated*image*in*");
+
+                if (line.Contains("reading prompts from"))
+                    Logger.Log("Generating...");
+
+                if (line.Contains("Decoding image: "))
+                {
+                    int percent = line.Split("Decoding image: ")[1].Split('#')[0].GetInt();
+
+                    if (percent > 0 && percent <= 100)
+                    {
+                        //Logger.Log($"Generating... {percent}%", false, replace);
+                        Program.MainForm.SetProgressImg(percent);
+                    }
+                }
+
+                if (line.MatchesWildcard("*data: 100%*<00:00,*it*]"))
+                {
+                    TextToImage.CurrentTask.ImgCount += 1;
+                    Program.MainForm.SetProgress((int)Math.Round(((float)TextToImage.CurrentTask.ImgCount / TextToImage.CurrentTask.TargetImgCount) * 100f));
+
+                    int lastMsPerImg = line.EndsWith("it/s]") ? (1000000f / (line.Split("00:00, ").Last().Remove(".").Remove("s") + "0").GetInt()).RoundToInt() : (line.Split("00:00, ").Last().Remove(".").Remove("s") + "0").GetInt();
+                    int remainingMs = (TextToImage.CurrentTask.TargetImgCount - TextToImage.CurrentTask.ImgCount) * lastMsPerImg;
+
+                    string lastLine = Logger.LastUiLine;
+
+                    Logger.Log($"Generated 1 image in {FormatUtils.Time(lastMsPerImg, false)} ({TextToImage.CurrentTask.ImgCount}/{TextToImage.CurrentTask.TargetImgCount})" +
+                        $"{(TextToImage.CurrentTask.ImgCount > 1 && remainingMs > 1000 ? $" - ETA: {FormatUtils.Time(remainingMs, false)}" : "")}", false, replace || Logger.LastUiLine.MatchesWildcard("Generated*image*"));
+                }
+            }
+
+
+            if (line.MatchesWildcard("*%|*/*[*B/s]*") && !line.ToLower().Contains("it/s") && !line.ToLower().Contains("s/it"))
+            {
+                Logger.Log($"Downloading required files... {line.Trunc(80)}", false, ellipsis);
+            }
+
+            if (line.MatchesWildcard("Added terms: *, *"))
+            {
+                if (line.MatchesWildcard("*, <*>"))
+                    Logger.Log($"Concept keyword: {line.Split("Added terms: *, ").LastOrDefault()}", false, ellipsis);
+                else
+                    Logger.Log($"Concept keyword: <{line.Split("Added terms: *, ").LastOrDefault()}>", false, ellipsis);
+            }
+
+            string lastLogLines = string.Join("\n", Logger.GetSessionLogLastLines("sd", 6));
+
+            if (!_hasErrored && line.Contains("CUDA out of memory"))
+            {
+                _hasErrored = true;
+                UiUtils.ShowMessageBox($"Your GPU ran out of VRAM! Try a lower resolution.\n\n{line.Split("If reserved memory is").FirstOrDefault()}", UiUtils.MessageType.Error);
+            }
+
+            if (!_hasErrored && (line.Contains("RuntimeError") || line.Contains("ImportError") || line.Contains("OSError")))
+            {
+                _hasErrored = true;
+                UiUtils.ShowMessageBox($"Python Error:\n\n{lastLogLines}", UiUtils.MessageType.Error);
+            }
+
+            if (!_hasErrored && (line.Contains("PytorchStreamReader failed reading zip archive") || line.Contains("UnpicklingError")))
+            {
+                _hasErrored = true;
+                UiUtils.ShowMessageBox($"Your model file seems to be damaged or incomplete!\n\n{lastLogLines}", UiUtils.MessageType.Error);
+            }
+
+            if (!_hasErrored && line.Contains("usage: "))
+            {
+                _hasErrored = true;
+                UiUtils.ShowMessageBox($"Invalid CLI syntax.", UiUtils.MessageType.Error);
+            }
+
+            if (!_hasErrored && line.ToLower().Contains("illegal memory access"))
+            {
+                _hasErrored = true;
+                UiUtils.ShowMessageBox($"Your GPU appears to be unstable! If you have an overclock enabled, please disable it!\n\n{line}", UiUtils.MessageType.Error);
+            }
+
+            // if (!_hasErrored && line.ToLower().Contains("0 image(s) generated in"))
+            // {
+            //     _hasErrored = true;
+            //     UiUtils.ShowMessageBox($"An unknown error occured. Check the log for details:!\n\n{lastLogLines}", UiUtils.MessageType.Error);
+            // }
+
+            if (_hasErrored)
+                TextToImage.Cancel();
+        }
+    }
+}
