ea3ea87f8f1377e461f44edd95c21ac153d1dafd
n00mkrad <login@nmkd.de>
2022-10-27 14:25:04 +0200

    Don't show VAE in log if none is used

--- StableDiffusionGui/Main/TtiProcess.cs
+++ StableDiffusionGui/Main/TtiProcess.cs
@@ -41,7 +41,7 @@ namespace StableDiffusionGui.Main
             string model = paramsDict.Get("model").FromJson<string>();
             bool hiresFix = paramsDict.Get("hiresFix").FromJson<bool>();
             bool lockSeed = paramsDict.Get("lockSeed").FromJson<bool>();
-            string vae = paramsDict.Get("vae").FromJson<string>();
+            string vae = paramsDict.Get("vae").FromJson<string>().Replace("None", "");
 
             if (!TtiUtils.CheckIfSdModelExists())
                 return;
