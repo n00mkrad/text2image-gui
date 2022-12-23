ff6015114d32f1ef1980af45f7caed16c98ad3b6
n00mkrad <login@nmkd.de>
2022-12-23 03:32:37 +0100

    Update MainUi.cs

--- StableDiffusionGui/Ui/MainUi.cs
+++ StableDiffusionGui/Ui/MainUi.cs
@@ -307,7 +307,7 @@ namespace StableDiffusionGui.Ui
             if (ver.Trim() != Program.Version)
                 Logger.Log($"It seems like you are not running the latest version.{(Program.ReleaseChannel == UpdateChannel.Public ? $" You can download the latest on itch: {Constants.Urls.ItchPage}" : "")}");
             else
-                Logger.Log($"You are running the latest version ({Program.ReleaseChannel}).");
+                Logger.Log($"You are running the latest version ({Program.ReleaseChannel} Channel).");
         }
 
         public static void SetSettingsVertScrollbar()
