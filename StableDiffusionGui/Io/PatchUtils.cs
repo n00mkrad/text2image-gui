bdd009d1e2f21ca00c7bb2b506502bb3dc7c4226
n00mkrad <login@nmkd.de>
2023-03-23 14:29:41 +0100

    Fix HF Hub patch

--- StableDiffusionGui/Io/PatchUtils.cs
+++ StableDiffusionGui/Io/PatchUtils.cs
@@ -16,13 +16,13 @@ namespace StableDiffusionGui.Io
         public static void PatchDiffusers () // Compatible as of Stax124/diffusers@5d41b5383c6a0421a24b17f4520d71a1834c1dac 
         {
             string diffRootPath = Path.Combine(Paths.GetDataPath(), Constants.Dirs.SdVenv, "lib", "site-packages", "diffusers");
-            ((Action)(() => PatchDiffusersConvCkpt(diffRootPath))).RunInTryCatch();
+            ((Action)(() => PatchDiffusersConvCkpt(diffRootPath))).RunInTryCatch("Patch Diffusers Exception:");
         }
 
         public static void PatchHuggingfaceHub() // Compatible as of 0.13.3
         {
             string hfRootPath = Path.Combine(Paths.GetDataPath(), Constants.Dirs.SdVenv, "lib", "site-packages", "huggingface_hub");
-            ((Action)(() => PatchHfDownloader(hfRootPath))).RunInTryCatch();
+            ((Action)(() => PatchHfDownloader(hfRootPath))).RunInTryCatch("Patch HF Hub Exception:");
         }
 
         private static void PatchDiffusersConvCkpt (string root)
@@ -38,7 +38,7 @@ namespace StableDiffusionGui.Io
 
         private static void PatchHfDownloader(string root)
         {
-            string scriptPath = Path.Combine(root, "pipelines", "stable_diffusion", "convert_from_ckpt.py");
+            string scriptPath = Path.Combine(root, "file_download.py");
 
             string textOld = File.ReadAllText(scriptPath);
             string textNew = textOld.Replace("_are_symlinks_supported_in_dir[cache_dir] = True", "_are_symlinks_supported_in_dir[cache_dir] = False");
