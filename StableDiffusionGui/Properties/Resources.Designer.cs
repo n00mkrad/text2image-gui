fe670285a05f9648ffa62ec604af84fd78adbd15
n00mkrad <login@nmkd.de>
2023-05-21 15:35:45 +0200

    Remove mona lisa test image from resources

--- StableDiffusionGui/Properties/Resources.Designer.cs
+++ StableDiffusionGui/Properties/Resources.Designer.cs
@@ -341,16 +341,6 @@ namespace StableDiffusionGui.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap mona512x384 {
-            get {
-                object obj = ResourceManager.GetObject("mona512x384", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream notify {
