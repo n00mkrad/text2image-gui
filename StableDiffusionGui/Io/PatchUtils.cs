74ab671e0953f32dd9ae9075d2364c93280a4c5c
n00mkrad <login@nmkd.de>
2023-06-12 11:31:19 +0200

    Add lycoris patcher to make it work with Diffusers >0.14

--- StableDiffusionGui/Io/PatchUtils.cs
+++ StableDiffusionGui/Io/PatchUtils.cs
@@ -7,13 +7,14 @@ namespace StableDiffusionGui.Io
 {
     internal class PatchUtils
     {
-        public static void PatchAllPkgs ()
+        public static void PatchAllPkgs()
         {
             PatchDiffusers();
             PatchHuggingfaceHub();
+            PatchLycoris();
         }
 
-        public static void PatchDiffusers () // Compatible as of Stax124/diffusers@5d41b5383c6a0421a24b17f4520d71a1834c1dac 
+        public static void PatchDiffusers() // Compatible as of Stax124/diffusers@5d41b5383c6a0421a24b17f4520d71a1834c1dac 
         {
             string diffRootPath = Path.Combine(Paths.GetDataPath(), Constants.Dirs.SdVenv, "lib", "site-packages", "diffusers");
             ((Action)(() => PatchDiffusersConvCkpt(diffRootPath))).RunInTryCatch("Patch Diffusers Exception:");
@@ -25,26 +26,61 @@ namespace StableDiffusionGui.Io
             ((Action)(() => PatchHfDownloader(hfRootPath))).RunInTryCatch("Patch HF Hub Exception:");
         }
 
-        private static void PatchDiffusersConvCkpt (string root)
+        public static void PatchLycoris() // Compatible as of 0.1.5
+        {
+            string lycRootPath = Path.Combine(Paths.GetDataPath(), Constants.Dirs.SdVenv, "lib", "site-packages", "lycoris");
+            ((Action)(() => PatchLycoris(lycRootPath))).RunInTryCatch("Patch Lycoris Exception:");
+        }
+
+        private static void PatchDiffusersConvCkpt(string root)
         {
             string scriptPath = Path.Combine(root, "pipelines", "stable_diffusion", "convert_from_ckpt.py");
+
+            if (!File.Exists(scriptPath))
+                return;
 
             string textOld = File.ReadAllText(scriptPath);
             string textNew = textOld.Replace("model_type == \"FrozenCLIPEmbedder\"", "model_type.endswith(\"FrozenCLIPEmbedder\")");
 
-            if (textNew != textOld)
-                File.WriteAllText(scriptPath, textNew);
+            if (textNew == textOld)
+                return;
+
+            File.WriteAllText(scriptPath, textNew);
+            Logger.LogHidden("Patched diffusers package");
         }
 
         private static void PatchHfDownloader(string root)
         {
             string scriptPath = Path.Combine(root, "file_download.py");
 
+            if (!File.Exists(scriptPath))
+                return;
+
             string textOld = File.ReadAllText(scriptPath);
             string textNew = textOld.Replace("_are_symlinks_supported_in_dir[cache_dir] = True", "_are_symlinks_supported_in_dir[cache_dir] = False");
 
-            if (textNew != textOld)
-                File.WriteAllText(scriptPath, textNew);
+            if (textNew == textOld)
+                return;
+
+            File.WriteAllText(scriptPath, textNew);
+            Logger.LogHidden("Patched HF package");
+        }
+
+        private static void PatchLycoris(string root)
+        {
+            string scriptPath = Path.Combine(root, "kohya.py");
+
+            if (!File.Exists(scriptPath))
+                return;
+
+            string textOld = File.ReadAllText(scriptPath);
+            string textNew = textOld.Replace("assert lora.lora_name not in names, f\"duplicated lora name: {lora.lora_name}\"", "continue");
+
+            if (textNew == textOld)
+                return;
+
+            File.WriteAllText(scriptPath, textNew);
+            Logger.LogHidden("Patched lycoris package");
         }
     }
 }
