210acc66a88fe9988eed7c119851dbd403f22d51
N00MKRAD <61149547+n00mkrad@users.noreply.github.com>
2023-07-28 14:36:32 +0200

    Publish 1.12.0b3

--- StableDiffusionGui/Program.cs
+++ StableDiffusionGui/Program.cs
@@ -16,7 +16,7 @@ namespace StableDiffusionGui
 {
     public static class Program
     {
-        public const string Version = "1.12.0b2";
+        public const string Version = "1.12.0b3";
         public const Enums.Program.UpdateChannel ReleaseChannel = Enums.Program.UpdateChannel.Beta;
 
         public static bool Debug { get { return Debugger.IsAttached || UserArgs.Get("debug").Lower() == true.ToString().Lower(); } }
